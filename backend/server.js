2e08d339e33f5d7b32c1a4dcbc8cafb93eb8cc41
NrgNinja <diazrenato2001@outlook.com>
2023-04-05 01:27:13 -0400

    Updating app for deployment
    Should now work in conjunction on Heroku.

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,15 @@ const userRoutes = require('./routes/user')
 const cors = require('cors')
 const bodyParser = require('body-parser');
 
-// const path = require('path');
+const path = require('path');
+
+// Serve static files from the frontend build folder
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// Catch-all route to serve the frontend's index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 // express app
 const app = express()
@@ -38,84 +46,3 @@ mongoose.connect(process.env.MONGODB_URI)
   .catch((error) => {
     console.log(error)
   })
-
-// if (process.env.NODE_ENV === 'production') 
-// {
-//   // Set static folder
-//   app.use(express.static('frontend/build'));
-
-
-//   app.get('*', (req, res) => 
-//   {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   });
-// }
-
-// const MongoClient = require('mongodb').MongoClient;
-// const client = new MongoClient(url);
-// client.connect(console.log("Mongo DB connected!"));
-
-// // const url = process.env.MONGODB_URI;
-// // const mongoose = require("mongoose");
-// // mongoose.connect(url)
-// //   .then(() => console.log("Mongo DB connected!"))
-// //   .catch(err => console.log(err));
-
-// var api = require('./api.js');
-// api.setApp(app, client);
-// // api.setApp(app, mongoose);
-
-
-// // routes
-// app.use('/api/workouts',workoutRoutes)
-// app.use('/api/user',workoutRoutes)
-
-
-// // connect to db
-// mongoose.connect(process.env.MONGO_URI)
-// .then(() => {
-//   app.listen(process.env.PORT, () => {
-//     console.log('connected to db & listening on port', process.env.PORT)
-//   })
-// })
-// .catch((error) => {
-//   console.log(error)
-// })
-
-// // middleware
-// app.use(express.json())
-// app.use((req, res, next) => {
-//   console.log(req.path,req.method)
-//   next()
-// })
-
-// if (process.env.NODE_ENV === 'production') 
-// {
-//   // Set static folder
-//   app.use(express.static('frontend/build'));
-
-
-//   app.get('*', (req, res) => 
-//   {
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   });
-// }
-
-// app.use((req, res, next) => 
-// {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET, POST, PATCH, DELETE, OPTIONS'
-//   );
-//   next();
-// });
-
-// app.listen(PORT, () => 
-// {
-//   console.log('Server listening on port ' + PORT);
-// });
