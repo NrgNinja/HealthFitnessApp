a042e94effd2754e88ba3d87f291fbdd9eb195d4
Sinttar <tristanhedrick.whs@gmail.com>
2023-05-01 13:40:12 -0400

    Home and image added

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,60 +1,83 @@
-// import React from 'react';
-
-// import PageTitle from '../components/PageTitle';
-// import LoggedInName from '../components/LoggedInName';
-// import CardUI from '../components/CardUI';
-
-// const CardPage = () =>
-// {
-//     return(
-//         <div>
-//             <PageTitle />
-//             <LoggedInName />
-//             <CardUI />
-//         </div>
-//     );
-// }
-
-// export default CardPage;
-import { useEffect }from 'react'
-import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
-import { useAuthContext } from "../hooks/useAuthContext"
+import { useEffect } from 'react';
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import { useAuthContext } from '../hooks/useAuthContext';
+import { motion as m } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
 
 // components
-import WorkoutDetails from '../components/WorkoutDetails'
-import WorkoutForm from '../components/WorkoutForm'
+import WorkoutDetails from '../components/WorkoutDetails';
+import WorkoutForm from '../components/WorkoutForm';
+import bigBoy from '../pizap.jpg';
 
 const Home = () => {
-  const {workouts, dispatch} = useWorkoutsContext()
-  const {user} = useAuthContext()
+  const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch('/api/workouts', {
-        headers: {'Authorization': `Bearer ${user.token}`},
-      })
-      const json = await response.json()
+        headers: { Authorization: `Bearer ${user.token}` },
+      });
+      const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: 'SET_WORKOUTS', payload: json})
+        dispatch({ type: 'SET_WORKOUTS', payload: json });
       }
-    }
+    };
 
     if (user) {
-      fetchWorkouts()
+      fetchWorkouts();
     }
-  }, [dispatch, user])
+  }, [dispatch, user]);
+
+  const [showImage, setShowImage] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowImage(!showImage);
+  };
 
   return (
-    <div className="home">
+    <m.div
+      className="home"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+    >
+      {/* Add button to bottom left corner */}
+      {!showImage && (
+        <button
+          className="image-button"
+          onClick={handleButtonClick}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          Press to see the peak of masculinity
+        </button>
+      )}
+      {showImage && (
+        <m.img
+          className="image"
+          src={bigBoy}
+          alt="My Image"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration:  8}}
+          onAnimationComplete={() => setShowImage(false)}
+        />
+      )}
       <div className="workouts">
-        {workouts && workouts.map((workout) => (
-          <WorkoutDetails key={workout._id} workout={workout} />
-        ))}
+        <WorkoutForm />
+        <AnimatePresence>
+          {workouts &&
+            workouts.map((workout) => (
+              <WorkoutDetails key={workout._id} workout={workout} />
+            ))}
+        </AnimatePresence>
       </div>
-      <WorkoutForm />
-    </div>
-  )
-}
+    </m.div>
+  );
+};
 
-export default Home
+export default Home;
