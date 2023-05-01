5a2d04df9b55b9e60528a36fb6ad749f57768b48
Sinttar <tristanhedrick.whs@gmail.com>
2023-04-30 21:26:28 -0400

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt');
+const { sendVerificationEmail } = require('../emailUtils');
 
 const createToken = (_id) => {
   return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
@@ -21,20 +23,35 @@ const loginUser = async (req, res) => {
   }
 }
 
+
 // signup a user
 const signupUser = async (req, res) => {
-  const {email, password} = req.body
-
   try {
-    const user = await User.signup(email, password)
+    const { email, password } = req.body;
 
-    // create a token
-    const token = createToken(user._id)
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ error: 'User already exists' });
+    }
 
-    res.status(200).json({email, token})
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the new user with hashed password
+    const user = await User.create({ email, password: hashedPassword });
+
+    // Generate a verification link and send it to the user's email
+    const verificationToken = jwt.sign({ userId: user._id }, process.env.SECRET, { expiresIn: '1h' });
+    const verificationLink = `${process.env.BASE_URL}/verify/${verificationToken}`;
+    await sendVerificationEmail(email, verificationLink);
+
+    return res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
-    res.status(400).json({error: error.message})
+    console.log(error);
+    return res.status(500).json({ error: 'Server error' });
   }
-}
+};
+
 
 module.exports = { signupUser, loginUser }
